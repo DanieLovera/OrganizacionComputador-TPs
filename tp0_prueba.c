f076486c9e42c7456ef5a3704ae5b0ae4b62cae1
moreover22 <amore@fi.uba.ar>
2020-10-12 23:05:18 -0300

    Se agrega decodificación

--- tp0_prueba.c
+++ tp0_prueba.c
@@ -7,7 +7,10 @@
 #include <stdbool.h>
 #include <stdint.h>
 #define _POSIX_C_SOURCE 200809L //para incluir getline
+
+
 char* BASE64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 
 void printHelp();
 void decode(FILE* inputFile, FILE* outputFile);
@@ -32,20 +35,87 @@ void codificar(char buffer[], int tamanio_buffer) {
 		mascaraBase = mascaraBase >> 6;	
 		_buffer[posicion++] = BASE64[caracter];
 	}
-	printf("%s\n", _buffer);
+	printf("%s", _buffer);
 }
 
-int main(int argc, char const *argv[]){
+int index_string(const char* string, char c) {
+	int index = 0;
+	char result;
+	while ((result = string[index++]) != '\0') {
+		if (result == c) 
+			return index;
+	}
+	return -1;
+}
+
+#define TAMANIO_BUFFER_RESULTADO 4
+
+void decodificar_bloque(char buffer[]) {
+	// char _buffer[5] = "====";
+	// char buffer[3] <- parametro
+	int indexes[TAMANIO_BUFFER_RESULTADO];
+	for (int i = 0; i < TAMANIO_BUFFER_RESULTADO; i++) {
+		indexes[i] = index_string(BASE64, buffer[i]) - 1;
+		printf("%c\n", BASE64[indexes[i]]);
+	}
+	// int mascaraBase;
+	// int caracter_binario;
+	// char caracter;
+	// int posicion = 0;
+    // mano
+	// m: 38
+	// a: 26
+	// n: 39
+	// o: 40
+	// 
+	// int mascara_limpieza = (0x00000000);
+	// << ((3 - tamanio_buffer) * 8);
+	// buffer_shifteado = buffer_shifteado | mascara_limpieza;
+
+	int bits_juntos = ((int) indexes[0] << (6 * 3)) | 
+					   ((int) indexes[1] << (6 * 2)) | 
+					   ((int) indexes[2] << (6 * 1)) | 
+					   ((int) indexes[3]);
+
+	char result[TAMANIO_BUFFER_RESULTADO];
+
+	result[0] = (bits_juntos >> 24) & 0xFF;
+	result[1] = (bits_juntos >> 16) & 0xFF;
+	result[2] = (bits_juntos >>  8) & 0xFF;
+	result[3] = (bits_juntos >>  0) & 0xFF;
+}
+
+void decodificar(char buffer[], int tamanio_buffer) {
+
+	decodificar_bloque(buffer);
+}
+
+int main(int argc, char const *argv[]) {
+	/*
+	char buffer[] = "TWFu";
+	int len = 4;
+
+	decodificar(buffer, len);
+
+	return 0;
+	*/
 
 	FILE *fp = argv[1] != NULL ? fopen(argv[1], "r") : stdin;
 	char buffer[4];
 	int tamanio_buffer;
-
-	while (!feof(fp)) {
-		buffer[3] = 0;
-		fread(buffer, 1, (sizeof(buffer) - 1), fp);
-		tamanio_buffer = strlen(buffer);
-		codificar(buffer,tamanio_buffer);
-	}	
+	bool keep_running = true;
+	while (keep_running) {
+		char c;
+		char buffer[4];
+		memset(buffer, 0, 4);
+		int index = 0;
+		while ((c = fgetc(fp)) != EOF) {    
+			buffer[index++] = c;
+			if (index >= 3) 
+				break;
+		}
+		keep_running = c != EOF;
+		codificar(buffer, index);
+	}
+	printf("\n");
 }
-
