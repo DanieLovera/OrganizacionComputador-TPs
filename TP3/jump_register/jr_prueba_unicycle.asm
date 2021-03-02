1dd7a771dfe4d8b7adcdcd14c5a1ca8e8f80a3fd
Daniel Lovera <daanilo2497@gmail.com>
2021-03-02 14:15:33 -0300

    Correccion de bug en salto con registros, se elimino el shift left entre ForkReg1 y MuxJumpReg

--- TP3/jump_register/jr_prueba_unicycle.asm
+++ TP3/jump_register/jr_prueba_unicycle.asm
@@ -1,9 +1,5 @@
-# Salto incondicional a la instruccion 4 del programa.
-# Por alguna extraña razon salta una instruccion mas alla de la indicada por el registro
-# En este caso deberia saltar a la misma linea del jr $t0 pero lo hace en la linea del addi $t2, $zero, 12
-# Parece tener logica si pensamos que el registro $t0 guarda la direccion del salto al que debe volver una
-# instruccion pero en la implementacion no se esperaba este comportamiento.
-addi $t0, $zero, 3
+# Salto incondicional a la instruccion 5 del programa.
+addi $t0, $zero, 20
 addi $t1, $zero, 8
 jr $t0
 addi $t2, $zero, 12
